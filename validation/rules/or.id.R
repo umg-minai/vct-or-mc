8edaef52779928ae0a15bbbbc699c24c2082e783
Sebastian Gibb <mail@sebastiangibb.de>
2024-07-09 15:18:12 +0200

    feat: accept only valid or.id

--- validation/rules/or.id.R
+++ validation/rules/or.id.R
@@ -1,3 +1,5 @@
 is.numeric(or.id)
 
 in_range(or.id, 1, 6)
+
+or.id %in% ref_settings$or.id[ref_settings$center.id %in% unique(center.id)]
--- validation/validate.R
+++ validation/validate.R
@@ -25,10 +25,11 @@ validate <- function(csv, yml, verbose = TRUE) {
     if (verbose)
         cat("\n===\n", basename(csv), sep = "")
 
+    set <- read.csv(frf("raw-data", "setting.csv"))
     dat  <- read.csv(csv, comment.char = "#", tryLogical = FALSE)
 
     vld <- rename_rules(validator(.file = yml))
-    cfr <- confront(dat, vld)
+    cfr <- confront(dat, vld, ref = list(ref_settings = set))
     smr <- summary(cfr)
 
     dat$Comment <- shorten(dat$Comment, 53)
