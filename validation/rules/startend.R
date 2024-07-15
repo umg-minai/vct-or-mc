3f417ba7d6b3e43de0978d50563f3fb69a883d2a
Sebastian Gibb <mail@sebastiangibb.de>
2024-07-15 11:07:58 +0200

    fix: avoid failing for NA dates

--- validation/rules/startend.R
+++ validation/rules/startend.R
@@ -1,8 +1,8 @@
 is.character(start)
 
-field_length(start, 19)
+!is.na(start) | field_length(start, 19)
 
-field_format(
+!is.na(start) | field_format(
     start,
     "202[45]-[0-1][0-9]-[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9]",
     type = "regex"
@@ -10,9 +10,9 @@ field_format(
 
 is.character(end)
 
-field_length(end, 19)
+!is.na(end) | field_length(end, 19)
 
-field_format(
+!is.na(end) | field_format(
     end,
     "202[45]-[0-1][0-9]-[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9]",
     type = "regex"
@@ -20,6 +20,6 @@ field_format(
 
 lubridate::ymd_hms(start) < lubridate::ymd_hms(end)
 
-lubridate::ymd_hms(start[-1]) >= lubridate::ymd_hms(end[-length(end)])
+(!is.na(start) & !is.na(end)) & lubridate::ymd_hms(start[-1]) >= lubridate::ymd_hms(end[-length(end)])
 
 lubridate::ymd_hms(start) > ymd_hms("2024-06-03 03:00:00")
