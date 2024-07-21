fe0b6eb163ee01fd2a9789dd82bf0ad181d6c2a9
Sebastian Gibb <mail@sebastiangibb.de>
2024-07-21 20:42:34 +0200

    feat: test range of old and new agc IDs

--- validation/rules/agc.id.R
+++ validation/rules/agc.id.R
@@ -3,3 +3,13 @@ is.character(agc.id)
 field_format(agc.id, '^C[HN][0-9]{10,}', type = 'regex')
 
 field_length(agc.id, n = 12)
+
+# the "old" agcs have an ID over 20000 and the new ones below 10000
+(
+    substr(agc.id, 2, 2) == "H" &
+    as.numeric(substr(agc.id, 7, 12)) > 20000
+) |
+(
+    substr(agc.id, 2, 2) == "N" &
+    as.numeric(substr(agc.id, 7, 12)) < 10000
+)
