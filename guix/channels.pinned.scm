9ad9a19ae02a899c7777f8dc8625ff7461049385
Sebastian Gibb <mail@sebastiangibb.de>
2024-08-22 21:41:52 +0200

    chore: upgrade guix channels to latest release

--- guix/channels.pinned.scm
+++ guix/channels.pinned.scm
@@ -3,7 +3,7 @@
         (url "https://github.com/umg-minai/guix-minai.git")
         (branch "main")
         (commit
-          "9cc424355f1477e47f2500008070ac200b90fcfb")
+          "59bb1bb222497fc6c0ae32f3a81bf6bbeb829c69")
         (introduction
           (make-channel-introduction
             "4e47c0f12cf838335bf731b1947ea434923569e3"
@@ -14,7 +14,7 @@
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "41db573403ac0ade033dfe6af09c187ebe6506b5")
+          "bef48dd553aef3c8fb3bc48143739debb629c825")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
