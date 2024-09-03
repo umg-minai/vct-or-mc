666ff02ec705314841d87c6cf4060df00e0f8d56
Sebastian Gibb <mail@sebastiangibb.de>
2024-09-03 11:49:43 +0200

    fix: readjust lower/upper bounds of agc.id

--- validation/rules/agc.id.R
+++ validation/rules/agc.id.R
@@ -4,12 +4,12 @@ field_format(agc.id, '^C[HN][0-9]{10,}', type = 'regex')
 
 field_length(agc.id, n = 12)
 
-# the "old" agcs have an ID over 20000 and the new ones below 10000
+# our "old" agcs have an ID over 9000 and the new ones below 12000
 (
     substr(agc.id, 2, 2) == "H" &
-    as.numeric(substr(agc.id, 7, 12)) > 20000
+    as.numeric(substr(agc.id, 7, 12)) > 9000
 ) |
 (
     substr(agc.id, 2, 2) == "N" &
-    as.numeric(substr(agc.id, 7, 12)) < 10000
+    as.numeric(substr(agc.id, 7, 12)) < 12000
 )
